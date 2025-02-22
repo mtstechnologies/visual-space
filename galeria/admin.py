5923cf4f1ebb5c5a0f325ce5b100667110bf6322
“mtstechnologies” <michael.trindade@hotmail.com>
2025-02-22 18:49:43 -0400

    tela de busca

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from galeria.models import Fotografia
 # Register your models here.
+
+
+class ListandoFotografias(admin.ModelAdmin):
+    # add titulo as colunas nas listas de objetos cadastraddos no django-admin
+    list_display = ('id', 'nome', 'legenda', 'publicada')
+    # tornando toda a linha da lista como LINK, ao clicar em qualquer campo, ira para a tela de edicao
+    list_display_links = ('id', 'nome', 'legenda')
+    # add campo de busca por nome
+    search_fields = ('nome',)
+    # add campo de filtro por categoria
+    list_filter = ('categoria',)
+    list_editable = ('publicada',)
+    # add paginacao
+    list_per_page = 10
+
+
+admin.site.register(Fotografia, ListandoFotografias)
--- setup/settings.py
+++ setup/settings.py
@@ -128,6 +128,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# diretorio de arquivos de MIDIA
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
